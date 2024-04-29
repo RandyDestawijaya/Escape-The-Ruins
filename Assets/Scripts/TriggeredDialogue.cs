390d6daa6b9ef0a1aa3350a8f1f7e8baf96cb95a
RandyDestawijaya <muhrandy87@gmail.com>
2024-04-29 20:44:20 +0700

    Add dialogue interaction

---
+++ Assets/Scripts/TriggeredDialogue.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
+
+public class TriggeredDialogue : MonoBehaviour
+{
+    public TextMeshProUGUI textComponent;
+    public string[] lines;
+    public float textSpeed;
+
+    private int index;
+
+    //[SerializeField]
+    // Achievement achievement;
+
+    //[SerializeField]
+    //AudioClip[] audioclips;
+    //AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //gameObject.SetActive(false);
+        //audioSource = GetComponent<AudioSource>();
+        //textComponent.text = string.Empty;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            if (textComponent.text == lines[index])
+            {
+                NextLine();
+                //audioSource.clip = audioclips[0];
+                //audioSource.Play();
+            }
+            else
+            {
+                StopAllCoroutines();
+                textComponent.text = lines[index];
+            }
+        }
+    }
+
+    public void StartDialogue()
+    {
+        gameObject.SetActive(true);
+        Time.timeScale = 0f;
+        index = 0;
+        textComponent.text = string.Empty;
+        StartCoroutine(TypeLine());
+        //audioSource.clip = audioclips[0];
+        //audioSource.Play();
+    }
+
+    IEnumerator TypeLine()
+    {
+        foreach (char c in lines[index].ToCharArray())
+        {
+            textComponent.text += c;
+            yield return new WaitForSecondsRealtime(textSpeed);
+        }
+    }
+
+    void NextLine()
+    {
+        if (index < lines.Length - 1)
+        {
+            index++;
+            textComponent.text = string.Empty;
+            StartCoroutine(TypeLine());
+        }
+        else
+        {
+            gameObject.SetActive(false);
+            Time.timeScale = 1f;
+            //achievement.StartDialogue();
+        }
+    }
+}
